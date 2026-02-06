45cf9e8e044ee64192b57539030fbb226e669381
soomin46.kim <urion46@gmail.com>
2026-02-06 15:31:25 +0900

    pracetice add

---
+++ Data_Struct/listAdt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+typedef int ElementType;
+
+typedef struct tagNode
+{
+    ElementType Data;
+    struct tagNode* NextNode;
+} Node;
+
+int main()
+{
+    // CreateNode, DestroyNode
+    // AppendNode
+    // GetNodeAt
+    // RemoveNode
+    // InsertAfter, InsertNewHead
+    
+    
+    
+    return 0;
+}
